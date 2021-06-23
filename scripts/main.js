3bb8d44fbf47c3e09fad0799a979f218a09e6dcb
Chris Hundhausen <hundhaus@wsu.edu>
2021-06-22 17:28:49 -0700

    Added better feedback when ref is redirected

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 window.onload = function () {
     let ref = window.location.search.substr(1);
     if (ref in refmap) {
+        document.getElementById("redirect").style.display = "block";
+        document.getElementById("refId").text = ref;
         location.href = refmap[ref];
     } else if (ref !== "") {
         document.getElementById("nFound").style.display = "block";
