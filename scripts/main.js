213b12ab62ee134559272c1599e7b21bff453024
Chris Hundhausen <hundhaus@wsu.edu>
2021-06-22 17:37:25 -0700

    Use textContent to set text of span element

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ window.onload = function () {
     let ref = window.location.search.substr(1);
     if (ref in refmap) {
         document.getElementById("redirect").style.display = "block";
-        document.getElementById("refId").text = ref;
+        document.getElementById("refId").textContent = ref;
         location.href = refmap[ref];
     } else if (ref !== "") {
         document.getElementById("nFound").style.display = "block";
